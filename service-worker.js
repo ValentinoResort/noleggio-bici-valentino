734237f1e214c83670c4bc258697840b5b8b7910
ValentinoResort <vresort620@gmail.com>
2025-04-23 18:00:25 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,14 @@
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('noleggio-bici-cache').then((cache) => {
-      return cache.addAll([
-        'noleggio_bici.html',
-        'logo.png',
-        'app_icon.png'
-      ]);
+    caches.open('bici-app-cache').then(cache => {
+      return cache.addAll(['index.html', 'app_icon.png']);
     })
   );
 });
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request);
     })
   );
